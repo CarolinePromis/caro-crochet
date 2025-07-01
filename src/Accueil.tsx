9f81dfd30839553e604c4638aa2c953415904767
Ouganda Knuckless <caroline.promis@etu.u-bordeaux.fr>
2025-07-01 19:15:51 +0200

    ajout d'un peu de contenu sur la page d'accueil (présentation du site) + quelques corrections de style

--- src/Accueil.tsx
+++ src/Accueil.tsx
@@ -1,7 +1,11 @@
 export function Accueil() {
     return (
         <>
-            <h1>Bienvenue sur l'accueil</h1>
+            <div className="glass-card p-5 text-center">
+                <h1>Caro Crochet</h1>
+                <h3>Bienvenue sur mon blog !<br/>Je poste ici tout ce que je fais en crochet, avec des conseils, des tutoriels et des infos pratiques, bonne lecture !</h3>
+                <button className="redirect-button">Conseils</button>
+            </div>
             {/* parallaxe avec des pelotes OU modèles 3D de pelotes qui tournent en fond, à voir */}
         </>
     );
--- src/App.tsx
+++ src/App.tsx
@@ -47,19 +47,18 @@ function App() {
           <div className="flex items-center relative h-16 justify-between">
 
             {/* Logo et nom du site */}
-            <div className="flex items-center">
+            <a href="/caro-crochet/" className="flex items-center">
               <img src={process.env.PUBLIC_URL + "/logo.png"} alt="Logo du site, une pelote de laine orange avec un C et un crochet blancs" width="50" />
-              <span className="text-2xl font-bold text-black dark:text-white ml-3">Caro Crochet</span>
-            </div>
+            </a>
 
             <div className="flex items-center space-x-4">
 
               {/* Icône light/dark mode */}
               <ThemeContext.Provider value={{ themeClass, setThemeClass }}>
                 <button className={themeClass} onClick={onHandleClick}>
-                    <svg fill={isDarkMode ? "#fff" : "#000"} viewBox="0 0 32 32" transform="rotate(-45)" strokeLinecap="round" strokeLinejoin="round" width={32} height={32} className="p-1 rounded-full fill-black hover:fill-white hover:bg-pink-500 dark:hover:bg-red-700 dark:fill-white dark:hover:fill-black">
-                      <path d="M30.994 13.84l-6.241-2.361 1.076-6.586-6.299 2.203-3.409-5.736-3.409 5.736-6.299-2.203 1.076 6.586-6.241 2.361 5.057 4.354-3.263 5.821 6.672 0.084 1.242 6.556 5.166-4.224 5.166 4.224 1.242-6.556 6.672-0.084-3.263-5.821 5.057-4.354zM16.472 25.494c-4.977 0-9.012-4.035-9.012-9.012s4.035-9.012 9.012-9.012c2.328 0 4.45 0.883 6.049 2.332-0.55-0.214-1.132-0.364-1.736-0.425-0.239-0.024-0.486 0-0.731 0-3.929 0-7.099 3.17-7.099 7.099s3.17 7.099 7.099 7.099c0.9 0 1.76-0.179 2.551-0.492-1.609 1.495-3.764 2.41-6.133 2.41z" />
-                    </svg>
+                  <svg fill={isDarkMode ? "#fff" : "#000"} viewBox="0 0 32 32" transform="rotate(-45)" strokeLinecap="round" strokeLinejoin="round" width={32} height={32} className="p-1 rounded-full fill-red-950 hover:fill-white hover:bg-pink-500 dark:hover:bg-red-700 dark:fill-white dark:hover:fill-red-950">
+                    <path d="M30.994 13.84l-6.241-2.361 1.076-6.586-6.299 2.203-3.409-5.736-3.409 5.736-6.299-2.203 1.076 6.586-6.241 2.361 5.057 4.354-3.263 5.821 6.672 0.084 1.242 6.556 5.166-4.224 5.166 4.224 1.242-6.556 6.672-0.084-3.263-5.821 5.057-4.354zM16.472 25.494c-4.977 0-9.012-4.035-9.012-9.012s4.035-9.012 9.012-9.012c2.328 0 4.45 0.883 6.049 2.332-0.55-0.214-1.132-0.364-1.736-0.425-0.239-0.024-0.486 0-0.731 0-3.929 0-7.099 3.17-7.099 7.099s3.17 7.099 7.099 7.099c0.9 0 1.76-0.179 2.551-0.492-1.609 1.495-3.764 2.41-6.133 2.41z" />
+                  </svg>
                 </button>
               </ThemeContext.Provider>
 
@@ -87,7 +86,7 @@ function App() {
           {open && (
             <div className="fixed w-full left-0 bg-pink-300 dark:bg-red-900 z-50 sm:hidden space-y-1 p-2" onMouseLeave={() => setOpen(false)}>
               {navigation.map((item) => (
-                <Link key={item.name} to={item.href} className="block rounded-md p-2 dark:text-white hover:bg-pink-500 dark:hover:bg-red-700" onClick={() => setOpen(false)}>
+                <Link key={item.name} to={item.href} className="block rounded-md p-2 hover:bg-pink-500 dark:hover:bg-red-700" onClick={() => setOpen(false)}>
                   {item.name}
                 </Link>
               ))}
@@ -105,33 +104,33 @@ function App() {
         </main>
       </BrowserRouter>
 
-      <footer className="text-black dark:text-white bg-sky-300 dark:bg-sky-500">
-        <div className="mx-auto p-5 flex space-x-10 flex-col md:flex-row justify-evenly items-center">
+      <footer className="text-black bg-sky-300 dark:bg-sky-800">
+        <div className="p-5 flex flex-col md:flex-row justify-evenly items-center">
           {/* Texte explicatif */}
-          <p className="my-3 md:max-w-md text-center leading-relaxed">
+          <p className="my-3 md:max-w-md text-center">
             <b>Pour les débutants comme pour les plus talentueux</b><br />
             J'ai commencé le crochet en autodidacte il y a quelque temps maintenant et j'adore créer mes propres modèles. C'est pour cela que j'ai pensé à créer ce site, où je publie mes modèles !
           </p>
 
           {/* Logos */}
           {/* GitHub */}
-          <div className="flex space-x-4 justify-center items-center">
-            <a href="https://github.com/CarolinePromis" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 dark:bg-sky-800 hover:bg-gray-600 dark:hover:bg-gray-900 rounded-lg flex items-center justify-center transition-all duration-300">
-              <svg className="w-7 h-7 dark:text-white" fill="currentColor" viewBox="0 0 24 24">
+          <div className="flex items-center space-x-5">
+            <a href="https://github.com/CarolinePromis" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 hover:bg-gray-600 dark:hover:bg-gray-900 rounded-lg flex items-center justify-center ">
+              <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z" />
               </svg>
             </a>
 
             {/* LinkedIn */}
-            <a href="https://www.linkedin.com/in/caroline-promis" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 dark:bg-sky-800 hover:bg-blue-500 dark:hover:bg-blue-600 rounded-lg flex items-center justify-center transition-all duration-300">
-              <svg className="w-5 h-5 dark:text-white" fill="currentColor" viewBox="0 0 24 24">
+            <a href="https://www.linkedin.com/in/caroline-promis" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 hover:bg-blue-500 dark:hover:bg-blue-600 rounded-lg flex items-center justify-center ">
+              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
               </svg>
             </a>
 
             {/* Ko-Fi */}
-            <a href="https://ko-fi.com/" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 dark:bg-sky-800 hover:bg-red-500 dark:hover:bg-red-600 rounded-lg flex items-center justify-center transition-all duration-300">
-              <svg className="w-6 h-6 dark:text-white" fill="currentColor" viewBox="0 0 24 24">
+            <a href="https://ko-fi.com/" target="_blank" rel="noreferrer" className="w-10 h-10 bg-sky-500 hover:bg-red-500 dark:hover:bg-red-600 rounded-lg flex items-center justify-center ">
+              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734 4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09-.443-.441-3.368-3.049-4.034-3.954-.709-.965-1.041-2.7-.091-3.71.951-1.01 3.005-1.086 4.363.407 0 0 1.565-1.782 3.468-.963 1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z" />
               </svg>
             </a>
