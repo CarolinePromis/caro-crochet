f1ce815c813b537e524462d8cb78291b3d688cee
Caroline <caropromis250@gmail.com>
2025-07-17 20:23:31 +0200

    navbar réparée

--- src/Accueil.tsx
+++ src/Accueil.tsx
@@ -2,6 +2,7 @@ export function Accueil() {
     return (
         <>
             <h1>Bienvenue sur l'accueil</h1>
+            {/* parallaxe avec des pelotes OU modèles 3D de pelotes qui tournent en fond, à voir */}
         </>
     );
 }
